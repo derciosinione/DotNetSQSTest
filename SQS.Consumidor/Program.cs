b2fc4e8487689725bdb03ac7dae76ef353be47d9
derciosinione <derciosinione@gmail.com>
2021-11-08 00:03:03 +0100

    Innitial commit

---
+++ SQS.Consumidor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Consumidor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string queeueUrl = "https://sqs.us-east-1.amazonaws.com/852704159394/drcash-logs-api-test";
+            
+            var client = new AmazonSQSClient(RegionEndpoint.USEast1);
+            var request = new ReceiveMessageRequest
+            {
+                QueueUrl = queeueUrl,
+                WaitTimeSeconds = 20
+            };
+            
+            var response = await client.ReceiveMessageAsync(request);
+
+            foreach (var message in response.Messages)
+            { 
+                Console.WriteLine(message.Body);
+                await client.DeleteMessageAsync(queeueUrl, message.ReceiptHandle);
+            }
+
+        }
+    }
+}
---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+
+namespace SQS.Fornecedor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new AmazonSQSClient(RegionEndpoint.USEast1);
+            var request = new SendMessageRequest
+            {
+                QueueUrl = "https://sqs.us-east-1.amazonaws.com/852704159394/drcash-logs-api-test",
+                MessageBody = "Testando Envio de mensagem na fila",
+            };
+            await client.SendMessageAsync(request);
+        }
+    }
+}
