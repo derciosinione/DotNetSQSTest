abe142232265576823de0d14fa6476fe0e61470f
derciosinione <derciosinione@gmail.com>
2021-11-08 16:01:48 +0100

    Edit: Changed name of variable queeueUrl to QueeueUrl

--- SQS.Consumidor/Consumidor.cs
+++ SQS.Consumidor/Consumidor.cs
@@ -11,14 +11,14 @@ namespace SQS.Consumidor
 {
     class Consumidor
     {
-        const string queeueUrl = "https://sqs.us-east-1.amazonaws.com/852704159394/drcash-logs-api-test";
+        private const string QueeueUrl = "https://sqs.us-east-1.amazonaws.com/852704159394/drcash-logs-api-test";
 
         static async Task Main(string[] args)
         {
             var client = new AmazonSQSClient(RegionEndpoint.USEast1);
             var request = new ReceiveMessageRequest
             {
-                QueueUrl = queeueUrl,
+                QueueUrl = QueeueUrl,
                 WaitTimeSeconds = 15
             };
             
@@ -28,7 +28,7 @@ namespace SQS.Consumidor
             foreach (var message in response.Messages)
             { 
                 user = JsonSerializer.Deserialize<Users>(message.Body);
-                await client.DeleteMessageAsync(queeueUrl, message.ReceiptHandle);
+                await client.DeleteMessageAsync(QueeueUrl, message.ReceiptHandle);
             }
             
             VerifyIfThereIsRecivedMessage(user, response.Messages.Any());
